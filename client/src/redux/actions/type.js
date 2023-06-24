a0ffdf885a7138b134541f55d786d0d097817170
megha2601 <109163494+megha2601@users.noreply.github.com>
2023-06-24 23:29:30 +0530

    Update type.js

--- client/src/redux/actions/type.js
+++ client/src/redux/actions/type.js
@@ -10,6 +10,6 @@ export const ALL_TODOS = 'All Todos';
 export const ACTIVE_TODOS = 'Active Todos';
 export const DONE_TODOS = 'Done Todos';
 export const TABS = [ALL_TODOS,
-    // ACTIVE_TODOS,
-    //DONE_TODOS
+    ACTIVE_TODOS,
+    DONE_TODOS
 ];
